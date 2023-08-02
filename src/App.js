6617943054c37d4ab45ac629132c918c4b1adbee
AlexandrIvanov <alexandr.1996@list.ru>
2023-08-02 16:51:17 +0300

    Use findBy, getBy, queryBy, userEvent and fireEvent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import {useEffect, useState} from 'react';
+
 function App() {
+    const [data, setData] = useState(null)
+    const [toggle, setToggle] = useState(false)
+    const [value, setValue] = useState('')
+
+    const onClick = () => setToggle(state => !state)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData({})
+        }, 100)
+    }, []);
+
     return (
         <div>
+            <h1 data-testid='value-element'>{value}</h1>
+            {toggle && <div data-testid='toggle-element'>toggle</div>}
+            {data && <div style={{color: 'red'}} className='block'>data element</div>}
             <h1>App Component</h1>
-            <button>Click</button>
-            <input type="text" placeholder="input placeholder..."/>
+            <button onClick={onClick} data-testid='toggle-btn'>Click</button>
+            <input
+                type="text"
+                placeholder="input placeholder..."
+                data-testid='input'
+                onChange={(e) => setValue(e.target.value)}
+            />
         </div>
     );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 describe('App tests', () => {
   test('renders learn react link', () => {
@@ -13,4 +14,62 @@ describe('App tests', () => {
     // expect(input).toBeInTheDocument();
     expect(input).toMatchSnapshot();
   });
+
+  // Используя query, мы можем убедиться, что какого-то элемента нет
+  test('query test', () => {
+    render(<App />);
+    const element = screen.queryByText(/hello/i);
+    expect(element).toBeNull()
+  })
+
+  // get должен найти какой-то элемент, если мы его не находим, то возникает ошибка
+  test.skip('get test', () => {
+    render(<App />);
+    const element = screen.getByText(/hello/i);
+    expect(element).toBeUndefined()
+  })
+
+  test('find test', async () => {
+    render(<App />);
+    // screen.debug()
+    const element = await screen.findByText(/data element/i);
+
+    expect(element).toBeInTheDocument()
+    expect(element).toHaveStyle({color: 'red'})
+    expect(element).toHaveClass('block')
+
+    // screen.debug()
+  })
+
+  test('click event', () => {
+    render(<App />);
+    const btn = screen.getByTestId('toggle-btn');
+
+    expect(btn).toBeInTheDocument()
+    expect(screen.queryByTestId('toggle-element')).toBeNull()
+
+    fireEvent.click(btn)
+    expect(screen.getByTestId('toggle-element')).toBeInTheDocument()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-element')).toBeNull()
+  })
+
+  test('input event', () => {
+    render(<App />);
+    const h1 = screen.getByTestId('value-element');
+    const input = screen.getByTestId('input');
+
+    expect(h1).toHaveTextContent('')
+
+    // fireEvent - искусственное событие
+    // fireEvent.input(input, {
+    //   target: {value: 'hello'}
+    // })
+
+    // userEvent - воспроизводит действия пользователя,
+    // обрабатываются события нажатия клавиш, движения мыши и т.д.
+    userEvent.type(input, 'hello')
+
+    expect(h1).toHaveTextContent('hello')
+  })
 })
