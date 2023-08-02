54cf9977e50f5654f70b50f9d1a6a69792276d4d
AlexandrIvanov <alexandr.1996@list.ru>
2023-08-02 10:29:29 +0300

    Add first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App tests', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const appElement = screen.getByText(/app component/i);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input placeholder/i);
+
+    expect(appElement).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    // expect(input).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+})
